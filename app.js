b8e4e42808b28891973c1c1f587cbee0f00d7e53
jarodr47 <jarodr47@asu.edu>
2022-04-15 22:22:20 -0400

    added some comments for documentation

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
-const apiKey = process.env.API_KEY;
+const express = require("express"); //express is an npm package we are using to manage our requests
+const bodyParser = require("body-parser"); //npm package to parse website body data on res and req
+const request = require("request"); //npm request package for HTTP
+const https = require("https"); //https package for https requests, mainly for API use. Axios would also work here
+const apiKey = process.env.API_KEY; //global variable to store heroku environment variable. To set use heroku CLI and do 'heroku config:set API_KEY=<apikey>'
 
 
-const app = express();
+const app = express(); //set our app to use express
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("public")); //this is so that we can pass static files on our responses such as styles.css and images
 
 app.get("/", function (req,res) {
     res.sendFile(__dirname+"/signup.html");
@@ -18,6 +18,7 @@ app.post("/", function (req,res) {
     const lName = req.body.lastName;
     const email = req.body.email;
     
+    //the data object is what we are passing to the mailchimp API call to create a new member
     let data = {
         members: [
             {
@@ -31,14 +32,17 @@ app.post("/", function (req,res) {
         ]
     };
 
+    //stringify our data object
     let jsonData = JSON.stringify(data);
 
+    //create API url and parameters
     const url = "https://us14.api.mailchimp.com/3.0/lists/b997b5a574"
     const options = {
         method: "POST",
         auth: `uname:${String(apiKey)}`
     }
 
+    //build our API call
     const request = https.request(url, options, function(response) {
         
         console.log(options.auth);
@@ -54,6 +58,7 @@ app.post("/", function (req,res) {
         // })
     })
 
+    //write and request our data
     request.write(jsonData);
     request.end();
 
